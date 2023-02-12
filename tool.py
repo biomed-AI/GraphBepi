423c84306a4fedcf9ad6b38aa74171d3c2474d3d
UtopiaFable <52177368+UtopiaFable@users.noreply.github.com>
2023-02-13 05:33:05 +0800

    Update tool.py

--- tool.py
+++ tool.py
@@ -5,17 +5,18 @@ import torch.nn.functional as F
 class METRICS:
     def __init__(self,device='cpu'):
         self.device=device
-        self.auroc=tm.AUROC().to(device)
-        self.prc=tm.PrecisionRecallCurve().to(device)
-        self.auc=tm.AUC(True).to(device)
-        self.roc=tm.ROC().to(device)
-        self.rec=tm.Recall().to(device)
-        self.prec=tm.Precision().to(device)
-        self.f1=tm.F1Score().to(device)
-        self.mcc=tm.MatthewsCorrCoef(num_classes=2).to(device)
-        self.bacc=tm.Accuracy(
-            num_classes=2,average='macro',multiclass=True
-        ).to(device)
+        self.auroc=tm.AUROC(task='binary').to(device)
+        self.auprc=tm.AveragePrecision(task='binary').to(device)
+        self.roc=tm.ROC(task='binary').to(device)
+        self.prc=tm.PrecisionRecallCurve(task='binary').to(device)
+        self.rec=tm.Recall(task='binary').to(device)
+        self.prec=tm.Precision(task='binary').to(device)
+        self.f1=tm.F1Score(task='binary').to(device)
+        self.mcc=tm.MatthewsCorrCoef(task='binary').to(device)
+        f=lambda a,b,c,d,e:(a/(a+d)+c/(b+c))/2
+        self.stat=tm.StatScores(task='binary').to(device)
+        self.bacc=lambda x,y:f(*self.stat(x,y))
+
     def to(self,pred,y):
         return pred.to(self.device),y.to(self.device)
     def calc_thresh(self,pred,y):
@@ -28,7 +29,7 @@ class METRICS:
         pred,y=self.to(pred,y)
         auroc = self.auroc(pred,y)
         prec, rec, th1 = self.prc(pred,y)
-        auprc = self.auc(rec,prec)
+        auprc = self.auprc(pred,y)
         fpr, tpr, th2 = self.roc(pred,y)
         return {
             'AUROC':auroc.cpu().item(),'AUPRC':auprc.cpu().item(),'prc':[rec[:-1],prec[:-1],th1],'roc':[fpr,tpr,th2]
@@ -37,7 +38,7 @@ class METRICS:
         pred,y=self.to(pred,y)
         auroc = self.auroc(pred,y)
         prec, rec, thresholds = self.prc(pred,y)
-        auprc = self.auc(rec,prec)
+        auprc = self.auprc(pred,y)
         if threshold is None:
             f1=(2*prec*rec/(prec+rec)).nan_to_num(0)[:-1]
             threshold = thresholds[torch.argmax(f1)]
@@ -45,7 +46,7 @@ class METRICS:
         self.f1.threshold=threshold
         self.rec.threshold=threshold
         self.mcc.threshold=threshold
-        self.bacc.threshold=threshold
+        self.stat.threshold=threshold
         self.prec.threshold=threshold
         f1 = self.f1(pred,y)
         rec = self.rec(pred,y)
